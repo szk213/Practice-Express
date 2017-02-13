70b0db1db637e6dc79e1d04226d7c1b4153705d4
szk213 <s.szk213@gmail.com>
2017-02-13 18:38:40 +0900

    passportバージョンアップ対応

--- Passport/config/passport.js
+++ Passport/config/passport.js
@@ -1,4 +1,5 @@
 var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // load up the user model
 var User = require('../app/models/user');
@@ -6,6 +7,9 @@ var config = require('../config/database'); // get db config file
 
 module.exports = function(passport) {
   var opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+  // opts.issuer = 'accounts.examplesoft.com';
+  // opts.audience = 'yoursite.net';
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findOne({id: jwt_payload.id}, function(err, user) {
